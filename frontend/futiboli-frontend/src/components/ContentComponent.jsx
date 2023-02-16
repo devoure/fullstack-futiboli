90cc8813b05b54d043061112f0984517213bae3e
Athumani Bakari <disguisedsandwich@gmail.com>
2023-02-16 19:51:35 +0300

    Frontend:Added Group side

--- frontend/futiboli-frontend/src/components/ContentComponent.jsx
+++ frontend/futiboli-frontend/src/components/ContentComponent.jsx
@@ -8,6 +8,7 @@ function ContentComponent(){
 
   const categories = ['EPL', 'UCL', 'Other Leagues', 'La Liga', 'World Cups']
   const slicedContent = blogContent.slice(0, 4)
+  const popularPost = blogContent.slice(0, 5)
   const blogContentCard = slicedContent.map((item)=>{
     return(
     <div className="blog-article">
@@ -52,6 +53,35 @@ function ContentComponent(){
       </div>
     )
   })
+
+  const popularPostCard = popularPost.map((item)=>{
+    return(
+      <div className="group-post-item">
+        <div className="group-post-item-image">
+          <div className="image">
+            <img src={ item.image } />
+          </div>
+          <div className="group-post-image-prev">
+            <div className="prev-date">
+              <ion-icon name="calendar-outline"></ion-icon>
+              <span>January 14, 2019</span>
+            </div>
+            <div className="prev-comments">
+              <span>2 Comments</span>
+            </div>
+          </div>
+        </div>
+        <div className="group-post-item-title">
+          <span>{ item.title }</span>
+        </div>
+      </div>
+    )
+  })
+  const tagsContentCard = categories.map((item)=>{
+    return (
+      <div className="tag-item">{ item }</div>
+    )
+  })
   return (
     <div className="content-blog-container">
       <div className="content-blog">
@@ -69,12 +99,38 @@ function ContentComponent(){
           </div>
 
           <div className="group-posts">
+            <div className="group-post-title">
+              <span>Popular Post</span>
+            </div>
+            <div className="group-post-content">
+              { popularPostCard }
+            </div>
           </div>
 
           <div className="group-newsletter">
+           <div className="newsletter-title">
+             <span>Newsletter</span>
+            </div>
+
+            <div className="newsletter-content">
+              <div className="newsletter-input">
+                <input type="text" placeholder="Email" />
+              </div>
+              <div className="newsletter-button">
+                <span>Subscribe</span>
+              </div>
+            </div>
           </div>
 
           <div className="group-tags">
+           <div className="tags-title">
+             <span>Popular Tags</span>
+            </div>
+
+            <div className="tags-content">
+              { tagsContentCard }
+            </div>
+
           </div>
         </div>
       </div>
